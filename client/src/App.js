32274e909e5d0d020fbda3c1ddee632957f1b62c
Dhruti014 <dhrutipatel1404@gmail.com>
2024-07-16 11:30:31 +0530

    welcome

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
+import Index from './modules/form/Index.js';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+        <Index/>
       </header>
     </div>
   );
---
+++ client/src/modules/form/Index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <div>
+        <h2>Welcome</h2>
+        <div className='name'>
+            {/* <input type='text'> */}
+            {/* </input> */}
+
+        </div>
+    </div>
+  )
+}
+
+export default Index
