91419af0c97f9a48198a27b644717df6796d5792
Jahnavee Patel <202101140@daiict.ac.in>
2024-07-17 11:57:51 +0530

    Dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
+import Index from './modules/form/Index.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Index/>
       </header>
     </div>
   );
---
+++ client/src/modules/form/Index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Index.css'
+
+function Index() {
+  return (
+    <div className='box'>
+        <h2>Welcome</h2>
+        <div className='name'>
+            {/* <input type='text'> */}
+            {/* </input> */}
+
+        </div>
+    </div>
+  )
+}
+
+export default Index
