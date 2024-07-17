3d2e5519159ec969842cf575419d5f57356005b3
Jahnavee Patel <202101140@daiict.ac.in>
2024-07-17 12:14:05 +0530

    Dashboard page-2

--- client/src/modules/Dashboard/Dashboard.js
+++ client/src/modules/Dashboard/Dashboard.js
@@ -3,6 +3,15 @@ import React from 'react';
 export const Dashboard = () => {
   return (
     <>
+    <div className='first'>
+
+    </div>
+    <div className='second'>
+
+    </div>
+    <div className='third'>
+        
+    </div>
     </>
   );
 }
