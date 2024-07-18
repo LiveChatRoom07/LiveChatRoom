a9fe154718b18b36644bc7fe7e865122894e5e0c
Jahnavee Patel <202101140@daiict.ac.in>
2024-07-18 15:27:44 +0530

    routing all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Dashboard from './modules/Dashboard/Dashboard';
 import Form from './modules/form/Index'
 import { Route, Routes } from 'react-router-dom';
-=======
-import ChangePassword from './modules/form/ChangePassword.js';
-import Dashboard from './modules/dashboard/Dashboard.js'
-import Index from './modules/form/Index.js';
-import { Routes } from 'react-router-dom';
-
->>>>>>> 4d16a79db05c9bfa373bbb47b00059317bb88cc7
 
 function App() {
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,26 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import {BrowserRouter} from 'react-router-dom';
-=======
-import { BrowserRouter } from 'react-router-dom';
->>>>>>> 4d16a79db05c9bfa373bbb47b00059317bb88cc7
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <BrowserRouter>
     <App />
     </BrowserRouter>
-=======
-
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    
->>>>>>> 4d16a79db05c9bfa373bbb47b00059317bb88cc7
   </React.StrictMode>
 );
 
