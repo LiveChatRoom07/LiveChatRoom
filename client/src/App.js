0ff49aa6ee55c66aecbf98252715c744683e5aea
Jahnavee Patel <202101140@daiict.ac.in>
2024-07-18 14:56:01 +0530

    issue due to audit fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Dashboard from './modules/Dashboard/Dashboard.js';
+import Form from './modules/form'
+import { Route, Router } from 'react-router-dom';
 function App() {
   return (
-    <div className="App" >
-      <Dashboard/>
-    </div>
+    <>
+    <Router>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/users/sign_in" element={<Form isSignInPage={true}/>} />
+      <Route path="/users/sign_up" element={<Form isSignInPage={false}/>} />
+    </Router>
+    </>
   );
 }
 
