26f87a03be8b31c29bbeeeb981f7567fa786d3f8
Dhruti014 <dhrutipatel1404@gmail.com>
2024-07-18 14:11:21 +0530

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import Index from './modules/form/Index.js';
 import Login from './modules/form/Login.js';
 import './App.css';
 import ChangePassword from './modules/form/ChangePassword.js';
+import Dashboard from './modules/dashboard/Dashboard.js'
 
 function App() {
+
   return (
     <div className="App" >
-      <ChangePassword/>
+      <Index/>
     </div>
   );
 }
--- client/src/components/Buttons/Button.js
+++ client/src/components/Buttons/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Button.css'
 
-const Button = ({label='button', type='button',disabled=false}) => {
+const Button = ({label='button', type='button', disabled=false}) => {
   return (
     <div>
         <button type={type} className='signin-button' disabled={disabled} >
