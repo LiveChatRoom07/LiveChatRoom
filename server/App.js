008f2b13deef0f7f8b8b42fa8f0885c3e8cbf708
Jahnavee Patel <202101140@daiict.ac.in>
2024-07-18 18:51:50 +0530

    temp -1

--- server/App.js
+++ server/App.js
@@ -11,6 +11,33 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.post('api/register', (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        if (!username || !email || !password) {
+            res.status(400).json({msg: 'Please enter all fields'});
+        }
+        else{
+            const isAlreadyExist = await User.findOne({email});
+            const isAlreadyExistuser = await User.findOne({username});
+            if(isAlreadyExist){
+                res.status(400).json({msg: 'User already exists'});
+            }
+            else if(isAlreadyExistuser){
+                res.status(400).json({msg: 'Username already exists'});
+            }
+            else{
+                const newUser= new User({username, email, password});
+            }
+
+        }
+
+    } catch (error) {
+        
+    }
+})
+
 app.listen(port, () => {
   console.log('Server is running on port' + port);
 })
