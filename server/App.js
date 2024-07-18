2f4a85cc2c17af0a822929282efcdfa5cac85bc2
Jahnavee Patel <202101140@daiict.ac.in>
2024-07-18 17:51:07 +0530

    creation of app.js

--- server/App.js
+++ server/App.js
@@ -1,3 +1,16 @@
 const express = require('express');
 
-export default Express;
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port' + port);
+})
