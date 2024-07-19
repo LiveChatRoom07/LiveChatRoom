351b83a94970cb3db30b46a9e8003e22a59182fd
Dhruti014 <dhrutipatel1404@gmail.com>
2024-07-19 12:09:29 +0530

    AuthenticationRouts_LoginToken

--- server/App.js
+++ server/App.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 require('./db/connection');
 const Users = require('./Models/Users');
@@ -44,7 +45,50 @@ app.post('/api/register', async(req, res, next) => {
         }
 
     } catch (error) {
-        
+        console.log('error:', error);
+    }
+})
+
+app.post('/api/login', async(req, res, next) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            res.status(400).json({msg: 'Please enter all fields'});
+        }
+        else{
+            const user = await Users.findOne({username});
+            if(!user){
+                res.status(400).json({msg: 'Username or password is incorrect'});
+            }
+            else{
+                const validUser= await bcryptjs.compare(password,user.password);
+                if(!validUser){
+                    res.status(400).json({msg: 'Username or password is incorrect'});
+                }else{
+                    const payload = {
+                        userid: user._id,
+                        name: user.username
+                    }
+                    const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'THIS_IS_A_JWT_SECRET_KEY';
+
+                    jwt.sign(payload, JWT_SECRET_KEY, {expiresIn: 86400}, async (err, token) => {
+                        await Users.updateOne({_id: user._id}, { 
+                            $set: { token } 
+                        })
+                        user.save();
+                        next();
+                    } )
+
+                    res.status(200).json({ user })
+                }
+                
+            }
+
+        }
+
+    } catch (error) {
+        console.log('error:', error);
     }
 })
 
